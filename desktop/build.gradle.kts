e77fbb0d20e11bb5d5d00eef073392c1d1eebb13
GatoGamer7DCP <marciaronaldo.de.vi@gmail.com>
2026-08-27 08:15:46 -0300

    alteração de lwjgl para libgdx

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -67,6 +67,10 @@ application {
     )
 }
 
+// =====================================================
+// NORMAL JAR
+// =====================================================
+
 tasks.jar {
     manifest {
         attributes(
@@ -74,4 +78,55 @@ tasks.jar {
                 "com.voxelteamgames.opendash.DesktopLauncherKt"
         )
     }
+}
+
+// =====================================================
+// OPEN DASH FAT JAR
+// =====================================================
+
+val fatJar =
+    tasks.register<Jar>("fatJar") {
+
+        archiveBaseName.set("OpenDash")
+        archiveVersion.set("0.1")
+        archiveClassifier.set("")
+
+        duplicatesStrategy =
+            DuplicatesStrategy.EXCLUDE
+
+        manifest {
+            attributes(
+                "Main-Class" to
+                    "com.voxelteamgames.opendash.DesktopLauncherKt"
+            )
+        }
+
+        // Inclui as classes e recursos do próprio Desktop
+        from(sourceSets.main.get().output)
+
+        // Inclui TODAS as dependências do runtime:
+        // Core + LibGDX + LWJGL + natives
+        dependsOn(configurations.runtimeClasspath)
+
+        from(
+            configurations.runtimeClasspath.get().map { file ->
+                if (file.isDirectory) {
+                    file
+                } else {
+                    zipTree(file)
+                }
+            }
+        )
+
+        // Arquivos de assinatura de JARs externos
+        // não podem ser carregados depois de misturados.
+        exclude(
+            "META-INF/*.SF",
+            "META-INF/*.RSA",
+            "META-INF/*.DSA"
+        )
+    }
+
+tasks.build {
+    dependsOn(fatJar)
 }
